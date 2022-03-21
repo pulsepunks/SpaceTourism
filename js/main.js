01d5c805fb4c652d687dffa349b1280c0de9e4d9
pulsepunks <75215808+pulsepunks@users.noreply.github.com>
2022-03-21 18:31:29 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// ----------Slicker----------
+$(function(){
+
+    $('.slider__box').slick({
+        prevArrow: '<img class="slider__arrow slider__arrow-prev" src="/img/slider/arrow-prev.svg" alt="Prev arrow">',
+        nextArrow: '<img class="slider__arrow slider__arrow-next" src="/img/slider/arrow-next.svg" alt="Next arrow">',
+
+    });
+
+
+});
